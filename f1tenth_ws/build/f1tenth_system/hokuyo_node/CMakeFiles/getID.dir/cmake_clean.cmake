30d7a14675e45b9dbdf8cd9077f26692fbcafac4
Cobeasta <stopyloknok@gmail.com>
2023-08-11 16:38:45 -0500

    first commit

---
+++ f1tenth_ws/build/f1tenth_system/hokuyo_node/CMakeFiles/getID.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/getID.dir/src/getID.cpp.o"
+  "/home/f1tenth/software/f1tenth_ws/devel/lib/hokuyo_node/getID.pdb"
+  "/home/f1tenth/software/f1tenth_ws/devel/lib/hokuyo_node/getID"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/getID.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ f1tenth_ws/build/f1tenth_system/hokuyo_node/CMakeFiles/libhokuyo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libhokuyo.dir/src/hokuyo.cpp.o"
+  "/home/f1tenth/software/f1tenth_ws/devel/lib/liblibhokuyo.pdb"
+  "/home/f1tenth/software/f1tenth_ws/devel/lib/liblibhokuyo.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libhokuyo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
